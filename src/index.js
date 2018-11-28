5b2895223c4dccb28191b62b0aed29921d2bb445
Douglas Bamber <douglasbamber@gmail.com>
2018-11-28 19:55:11 +1300

    ADD ability to not specify a fields array, and use the first objects keys as default

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import get from 'lodash.get';
  * where any their specified `fields` include the every word in the `term` string.
  * @param  {Array<Object>} collection The collection to iterate over.
  * @param  {string} term The search term.
- * @param  {Array<string>} fields The element's fields to be searched.
+ * @param  {Array<string>} [fields] The element's fields to be searched.
  * @return {Array} Returns the new filtered array.
  */
 function search(collection, term, fields) {
+  if (!collection || !collection.length) {
+    return [];
+  }
+  // The problem with this is it assumes all objects have the same keys
+  const fieldsToSearch = fields ? fields : Object.keys(collection[0]);
+
   if (!term) return collection;
   const terms = term.split(' ');
   return collection.filter((value) => {
-    const values = fields.map(field => get(value, field)).join().toLowerCase();
+    const values = fieldsToSearch.map(field => get(value, field)).join().toLowerCase();
     return terms.reduce((accumulator, searchTerm) => {
       if (!accumulator) return false;
       return values.indexOf(searchTerm.toLowerCase()) >= 0;
