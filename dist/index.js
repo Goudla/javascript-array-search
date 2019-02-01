6412be1cc82ab0e81bd816cec4e5212892ca1d19
Douglas Bamber <douglasbamber@gmail.com>
2019-02-02 10:17:55 +1300

    ADD mocha dep and test file

--- dist/index.js
+++ dist/index.js
@@ -3,27 +3,39 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _lodash = require('lodash.get');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function get(object, path) {
+  var pathArray = path.split('.');
+  if (!pathArray.length) {
+    return;
+  }
+  if (pathArray.length === 1) {
+    return object[pathArray[0]];
+  }
+  return pathArray.reduce(function (acc, cur) {
+    return acc[cur];
+  }, object);
+}
 
 /**
  * Iterates over elements of `collection`, returning an array of all elements
- * where their fields include the search term.
- * @param  {Array} collection The collection to iterate over.
+ * where any their specified `fields` include the every word in the `term` string.
+ * @param  {Array<Object>} collection The collection to iterate over.
  * @param  {string} term The search term.
- * @param  {Array<string>} fields The element's fields to be searched.
+ * @param  {Array<string>} [fields] The element's fields to be searched.
  * @return {Array} Returns the new filtered array.
  */
 function search(collection, term, fields) {
+  if (!collection || !collection.length) {
+    return [];
+  }
+  // The problem with this is it assumes all objects have the same keys
+  var fieldsToSearch = fields ? fields : Object.keys(collection[0]);
+
   if (!term) return collection;
   var terms = term.split(' ');
   return collection.filter(function (value) {
-    var values = fields.map(function (field) {
-      return (0, _lodash2.default)(value, field);
+    var values = fieldsToSearch.map(function (field) {
+      return get(value, field);
     }).join().toLowerCase();
     return terms.reduce(function (accumulator, searchTerm) {
       if (!accumulator) return false;
